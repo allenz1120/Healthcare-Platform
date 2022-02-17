2a32ca08bc978bda8029b362ab01c8fc18a7575d
allenz1120 <55994268+allenz1120@users.noreply.github.com>
2022-02-17 11:04:54 -0500

    Added a random UID field in users

--- mysite/myapi/models.py
+++ mysite/myapi/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+import uuid
 
 # Create your models here.
+
+
 class Users(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False)
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     date_of_birth = models.DateField()
     address = models.CharField(max_length=100)
     sex = models.CharField(max_length=10)
     pcp = models.CharField(max_length=50)
+
+# class Billing(models.Model):
--- mysite/myapi/serializers.py
+++ mysite/myapi/serializers.py
@@ -5,4 +5,4 @@ from .models import Users
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Users
-        fields = ('first_name', 'last_name', 'date_of_birth', 'address', 'sex', 'pcp')
+        fields = ('id','first_name', 'last_name', 'date_of_birth', 'address', 'sex', 'pcp')
